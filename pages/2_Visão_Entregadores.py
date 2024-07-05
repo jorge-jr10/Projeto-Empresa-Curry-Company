0392a2328bc81805d8fe4e290882501a33d93d1d
Jorge Junior <jorge.jv321@gmail.com>
2024-07-05 00:27:09 -0300

    Update 2_Visão_Entregadores.py

--- pages/2_Visão_Entregadores.py
+++ pages/2_Visão_Entregadores.py
@@ -83,7 +83,7 @@ def clean_code(df1):
 
 
 # import dataset 
-df = pd.read_csv('C:\\Users\\t51269773852\\OneDrive - DIAGNOSTICOS DA AMERICA S.A\\Documentos\\Repos\\dataset\\archive\\train.csv')
+df = pd.read_csv('pages/dataset/train.csv')
 
 #limpando o dataset
 df1 = clean_code( df )
